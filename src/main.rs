d2ec71d06ab812fcb36e464866716ba5de147e03
Tobias <tobias.feld@hotmail.com>
2022-12-04 21:08:04 +0000

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::fs;
 
+use itertools::Itertools;
 use substring::Substring;
 
 fn main() {
     let string = fs::read_to_string("input.txt").expect("File");
 
+    let groups = string.split("\n").tuples::<(&str, &str, &str)>();
+
+    let result = groups
+        .map(|group| {
+            let output = get_same_in_three_rs(group.0, group.1, group.2).unwrap();
+            let value = get_value_from_char(output);
+            return value;
+        })
+        .fold(0, |a, b| a + b);
+
+    println!("{}", result);
+}
+
+#[allow(dead_code)]
+fn part_one(string: String) {
     let score = string
         .split("\n")
         .map(|bags| {
@@ -32,6 +48,22 @@ fn get_same_in_both_rs(a: &str, b: &str) -> Option<char> {
         for c2 in b.chars() {
             if c.eq(&c2) {
                 char = Some(c);
+            }
+        }
+    }
+    return char;
+}
+
+fn get_same_in_three_rs(a: &str, b: &str, x: &str) -> Option<char> {
+    let mut char = Option::None;
+    for c in a.chars() {
+        for c2 in b.chars() {
+            for c3 in x.chars() {
+                for c2 in b.chars() {
+                    if c.eq(&c2) && c.eq(&c3) {
+                        char = Some(c);
+                    }
+                }
             }
         }
     }
