18e2364755353e57aeed96763e979d5205550299
Tobias <tobias.feld@hotmail.com>
2022-12-06 12:17:27 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let mut queue = VecDeque::<char>::new();
     for (index, char) in input.chars().enumerate() {
         queue.push_back(char);
-        if queue.len() > 4 {
+        if queue.len() > 14 {
             queue.pop_front();
             if is_unique(&queue) == false {
                 println!("Unique string at {}: {:?}", index + 1, queue);
@@ -23,11 +23,11 @@ fn main() {
 
 fn is_unique(c: &VecDeque<char>) -> bool {
     let mut set = HashSet::<char>::new();
-    if c.len() < 4 {
+    if c.len() < 14 {
         return true;
     }
-    for i in 0..4 {
+    for i in 0..14 {
         set.insert(*c.get(i).unwrap());
     }
-    return set.len() != 4;
+    return set.len() != 14;
 }
