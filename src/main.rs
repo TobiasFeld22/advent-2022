5f8de0e2491093c191c28a8fe1f1123e00394b1f
Tobias <tobias.feld@hotmail.com>
2022-12-06 12:12:45 +0000

    part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    fs,
+};
+
 fn main() {
     println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("File input");
+
+    let mut queue = VecDeque::<char>::new();
+    for (index, char) in input.chars().enumerate() {
+        queue.push_back(char);
+        if queue.len() > 4 {
+            queue.pop_front();
+            if is_unique(&queue) == false {
+                println!("Unique string at {}: {:?}", index + 1, queue);
+                break;
+            }
+        }
+        println!("{:?} | {}", queue, is_unique(&queue));
+    }
+}
+
+fn is_unique(c: &VecDeque<char>) -> bool {
+    let mut set = HashSet::<char>::new();
+    if c.len() < 4 {
+        return true;
+    }
+    for i in 0..4 {
+        set.insert(*c.get(i).unwrap());
+    }
+    return set.len() != 4;
 }
